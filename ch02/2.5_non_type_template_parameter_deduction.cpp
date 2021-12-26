6fa936142749819d85834fc977099cb57e874dad
Lihe Chen <lihe.chen@gmail.com>
2021-12-26 14:14:14 -0600

    2.5_non_type_template_parameter_deduction
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

---
+++ ch02/2.5_non_type_template_parameter_deduction.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+// type template parameter
+template<typename T, typename U>
+auto add(T t, U u) {
+    return t+u;
+}
+
+// non-type template parameter
+template<typename T, int BufSize>
+class buffer_t {
+    public:
+        T& alloc();
+        void free(T& item);
+    private:
+        T data[BufSize];
+};
+
+//usage: 
+buffer_t<int, 100> buf;
+
+template<auto value> void foo() {
+    std::cout << value << std::endl;
+}
+
+int main() {
+    foo<1001>();
+}
