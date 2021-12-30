1ea7696631146896fe6beeeb633d37d25bd5de7c
Lihe Chen <lihe.chen@gmail.com>
2021-12-30 10:54:34 -0600

    3.3 perfect forwarding
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

---
+++ ch03/3.3_perfect_forwarding.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <utility>
+
+void reference(int& v) {
+    std::cout << "lvalue reference" << std::endl;
+}
+
+void reference(int&& v) {
+    std::cout << "rvalue reference" << std::endl;
+}
+
+template<typename T>
+void pass(T&& v) {
+    std::cout << " normal param passing: ";
+    reference(v);
+}
+
+template<typename T>
+void pass_better(T&& v) {
+    std::cout << " normal param passing: ";
+    reference(v);
+
+    std::cout << " std::move param passing: ";
+    reference(std::move(v));
+
+    std::cout << " std::forward param passing: ";
+    reference(std::forward<T>(v));
+
+    std::cout << " static_cast<T&&> param passing: ";
+    reference(static_cast<T&&>(v));
+}
+
+int main()  {
+    // the issue and reference collapse rule
+    std::cout << "rvalue pass:" << std::endl;
+    pass(1);
+    std::cout << "lvalue pass:" << std::endl;
+    int l = 1;
+    pass(l);
+
+    std::cout << "-----pass_better-----" << std::endl;
+    std::cout << "rvalue pass:" << std::endl;
+    pass_better(1);
+    std::cout << "lvalue pass:" << std::endl;
+    // int l = 1;
+    pass_better(l);
+
+
+    return 0;
+}
