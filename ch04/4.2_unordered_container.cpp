2e7953f89f7148d17fb3f3c707c6b5a0c216a11c
Lihe Chen <lihe.chen@gmail.com>
2022-01-01 07:55:10 -0600

    4.2_unordered_container.cpp
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

---
+++ ch04/4.2_unordered_container.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <map>
+
+int main() {
+    std::unordered_map<int, std::string> u = {
+        {1, "1"}, {2, "2"}, {3, "3"}
+    };
+
+    std::map<int, std::string> v = {
+        {1, "1"}, {2, "2"}, {3, "3"}
+    };
+
+    std::cout << "std::unordered_map" << std::endl;
+    for (const auto & n: u) {
+        std::cout << "Key[" << n.first << "]=[" << n.second << "]" << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "std::map" << std::endl;
+    for (const auto & n: v) {
+        std::cout << "Key[" << n.first << "]=[" << n.second << "]" << std::endl;
+    }
+}
